9cb8807ce653db37f944076e02b8688bf58ec5a2
Voles <niels.dequeker@gmail.com>
2013-04-20 18:10:34 +0200

    Added source code

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by JetBrains PhpStorm.
+ * User: nielsdequeker
+ * Date: 26/11/11
+ * Time: 19:46
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * DOM ready
+ */
+$(document).ready(function ()
+{
+	$('#compare').beforeAfter();
+	
+});
